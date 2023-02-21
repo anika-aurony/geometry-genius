64831be56141d0777e1faaf5d852ba9cb4fcba18
anikaaurony <aurony111@gmail.com>
2023-02-21 19:25:52 +0600

    Result display  done

--- script/script.js
+++ script/script.js
@@ -7,14 +7,7 @@ function getInput(input){
     
 }
 
-// Triangle
-document.getElementById('triangleButton').addEventListener('click', function(){
-    const triangleb = getInput('triangleB');
-    const triangleh = getInput('triangleH');  
-    const triangleArea = .5*triangleb*triangleh;
-    const triangleResult = triangleArea.toFixed(2)
-    console.log(triangleArea);
-    ///
+function setOutput(output, p){
     const container = document.getElementById('orderList');
     const li = document.createElement('li');
     const span = document.createElement('span');
@@ -22,16 +15,17 @@ document.getElementById('triangleButton').addEventListener('click', function(){
     const sup= document.createElement('sup');
     const sup1= document.createElement('sup');
     const button= document.createElement('button');
-    button.classList.add('btn')
-    button.classList.add('btn-info')
-    button.classList.add('w-50')
-    button.classList.add('p-0')
-    button.classList.add('ms-1')
-    const p = 'Triangle  '
+    button.classList.add('btn');
+    button.classList.add('btn-info');
+    button.classList.add('w-50');
+    button.classList.add('p-0');
+    button.classList.add('ms-2');
+    li.classList.add('mb-2');
+
     li.innerText = p;
-    span.innerText = triangleResult;
+    span.innerText = output;
     li.appendChild(span);
-    const unit = 'cm'
+    const unit = ' cm'
     const power = '2' ;
     span1.innerText = unit;
     sup.innerText = power;
@@ -42,6 +36,20 @@ document.getElementById('triangleButton').addEventListener('click', function(){
     button.appendChild(sup1);
     li.append(button)
     container.appendChild(li)
+
+}
+
+// Triangle
+document.getElementById('triangleButton').addEventListener('click', function(){
+    const triangleb = getInput('triangleB');
+    const triangleh = getInput('triangleH');  
+    const triangleArea = .5*triangleb*triangleh;
+    const triangleResult = triangleArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Triangle  '
+    setOutput(triangleResult, p)
+    
 })
 
 // Rectangle
@@ -49,7 +57,11 @@ document.getElementById('rectangleButton').addEventListener('click', function(){
     const rectangleW = getInput('rectangleW');
     const rectangleL = getInput('rectangleL');   
     const rectangleArea = rectangleW*rectangleL;
-    console.log(rectangleArea);
+    const rectangleResult = rectangleArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Rectangle '
+    setOutput(rectangleResult, p)
 });
 
 //Paralellogram
@@ -57,7 +69,11 @@ document.getElementById('paralellogramButton').addEventListener('click', functio
     const paralellogramB = getInput('paralellogramB')
     const paralellogramH = getInput('paralellogramH')
     const paralellogramArea = paralellogramB * paralellogramH;
-    console.log(paralellogramArea)
+    const paralellogramResult = paralellogramArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Paralellogram '
+    setOutput(paralellogramResult, p)
 })
 
 // Rhombus
@@ -65,7 +81,11 @@ document.getElementById('rhombusButton').addEventListener('click', function(){
     const rhombusD1 = getInput('rhombusD1')
     const rhombusD2 = getInput('rhombusD2')
     const rhombusArea = 0.5 * rhombusD1 * rhombusD2;
-    console.log(rhombusArea)
+    const rhombusResult = rhombusArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Rhombus '
+    setOutput(rhombusResult, p)
 })
 
 
@@ -74,7 +94,11 @@ document.getElementById('pentagonButton').addEventListener('click', function(){
     const pentagonP = getInput('pentagonP')
     const pentagonB = getInput('pentagonB')
     const pentagonArea = 0.5 * pentagonP * pentagonB;
-    console.log(pentagonArea);
+    const pentagonResult = pentagonArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Pentagon '
+    setOutput(pentagonResult, p)
 })
 
 //Ellipse
@@ -82,7 +106,11 @@ document.getElementById('ellipseButton').addEventListener('click', function(){
     const ellipseA = getInput('ellipseA')
     const ellipseB = getInput('ellipseB')
     const ellipseArea = 3.14 * ellipseA * ellipseB;
-    console.log(ellipseArea);
+    const ellipseResult = ellipseArea.toFixed(2)
+    
+    // Display Result
+    const p = 'Ellipse '
+    setOutput(ellipseResult, p)
 })
 
 
